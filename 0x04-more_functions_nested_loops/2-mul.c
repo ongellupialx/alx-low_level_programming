7eab7f67a83c257e02fafdfd5da76296afcb95ca
Anas <chopizam@gmail.com>
2023-06-26 04:54:35 -0700

    Pointers, arrays and strings

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 /**
- *  * mul - multiplies two integers
- *   * @a: first integer
- *    * @b: second integer
- *     *
- *      * Return: product of a and b
- *       */
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
 int mul(int a, int b)
 {
 	return (a * b);
